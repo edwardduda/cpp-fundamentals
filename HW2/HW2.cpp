437c71a0d36ad795e6fabd1debcc323053355a0f
Edward.Duda <edwardd@colostate.edu>
2025-02-16 20:46:36 -0700

    Modify validate_image method
    Add more  specific errors so it's easier to debug

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -6,6 +6,7 @@
 std::vector<std::vector<char>> create_image_v(const std::string &img_file);
 void printMatrix(const std::vector<std::vector<char>> &matrix);
 std::vector<char> create_check_sum_v(const std::string &checksum_file);
+int validate_image(const std::vector<std::vector<char>> &image, const std::vector<char> checksum);
 
 int main(int argc, char* argv[]) {
     if(argc != 3){
@@ -13,16 +14,44 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     std::vector<std::vector<char>> image = create_image_v(argv[1]);
+    std::vector<char> checksum = create_check_sum_v(argv[2]);
+    
+    validate_image(image, checksum);
 
     printMatrix(image);
 
+}
+
+int validate_image(const std::vector<std::vector<char>> &image, const std::vector<char> checksum){
+    
+    if(image.size() != checksum.size()){
+        std::cerr << "The image and checksum need the same number of rows." << std::endl;
+        return -2;
+    }
+    
+    for(size_t row = 0; row < image.size(); row++){
+        int sum = 0;
+        for(size_t column; column < image.size(); column++){
+            sum += image[row][column];
+        }
+
+        if(sum != checksum[row]){
+            std::cerr << "Image row and check row aren't equal. Expect sum: "
+            << checksum[row]
+            << "Actual: "
+            << sum;
+            return -3;
+        }
+        
+    }
+    return 0;
 }
 std::vector<char> create_check_sum_v(const std::string &checksum_file){
     std::vector<char> check_sum_v;
 
     std::fstream file(checksum_file);
     if(!file){
-        std::cerr << "Not able to open file.";
+        std::cerr << "Not able to open file." << std::endl;
         return check_sum_v;
     }
 
@@ -42,23 +71,29 @@ std::vector<std::vector<char>> create_image_v(const std::string &img_file){
 
     std::fstream file(img_file);
     if(!file){
-        std::cerr << "Not able to open file.";
+        std::cerr << "Not able to open file." << std::endl;
         return ascii_vector;
     }
     std::string line;
+    int line_count = 0;
     while (std::getline(file, line)){
+        if (line_count < 2) {
+            line_count++;
+            continue;
+        }
         ascii_vector.push_back(std::vector<char>(line.begin(), line.end()));
     }
 
     file.close();
-
+    
     return ascii_vector;
 }
+
 
 void printMatrix(const std::vector<std::vector<char>>& matrix) {
     for (const auto& row : matrix) {
         for (char c : row) {
-            std::cout << c;
+            std::cout << c ;
         }
         std::cout << std::endl;
     }
