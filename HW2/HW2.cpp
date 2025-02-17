b9fc2018301c5f140f13cea48162c9352fd95869
Edward.Duda <edwardd@colostate.edu>
2025-02-16 22:01:10 -0700

    Change char vectors to int vectors

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -3,18 +3,18 @@
 #include <vector>
 #include <string>
 
-std::vector<std::vector<char>> create_image_v(const std::string &img_file);
+std::vector<std::vector<int>> create_image_v(const std::string &img_file);
 void printMatrix(const std::vector<std::vector<char>> &matrix);
-std::vector<char> create_check_sum_v(const std::string &checksum_file);
-int validate_image(const std::vector<std::vector<char>> &image, const std::vector<char> checksum);
+std::vector<int> create_check_sum_v(const std::string &checksum_file);
+int validate_image(const std::vector<std::vector<int>> &image, const std::vector<int> checksum);
 
 int main(int argc, char* argv[]) {
     if(argc != 3){
         std::cout << "Not enought arguments\n";
         return 1;
     }
-    std::vector<std::vector<char>> image = create_image_v(argv[1]);
-    std::vector<char> checksum = create_check_sum_v(argv[2]);
+    std::vector<std::vector<int>> image = create_image_v(argv[1]);
+    std::vector<int> checksum = create_check_sum_v(argv[2]);
     
     validate_image(image, checksum);
 
@@ -46,8 +46,9 @@ int validate_image(const std::vector<std::vector<char>> &image, const std::vecto
     }
     return 0;
 }
-std::vector<char> create_check_sum_v(const std::string &checksum_file){
-    std::vector<char> check_sum_v;
+
+std::vector<int> create_check_sum_v(const std::string &checksum_file){
+    std::vector<int> check_sum_v;
 
     std::fstream file(checksum_file);
     if(!file){
@@ -55,8 +56,8 @@ std::vector<char> create_check_sum_v(const std::string &checksum_file){
         return check_sum_v;
     }
 
-    char row_sum;
-    while (file.get(row_sum)) {
+    int row_sum;
+    while (file >> row_sum) {
         check_sum_v.push_back(row_sum);
     }
 
@@ -65,23 +66,28 @@ std::vector<char> create_check_sum_v(const std::string &checksum_file){
     return check_sum_v;
 }
 
-std::vector<std::vector<char>> create_image_v(const std::string &img_file){
+std::vector<std::vector<int>> create_image_v(const std::string &img_file){
     
-    std::vector<std::vector<char>>ascii_vector;
+    std::vector<std::vector<int>>ascii_vector;
 
     std::fstream file(img_file);
     if(!file){
         std::cerr << "Not able to open file." << std::endl;
         return ascii_vector;
     }
-    std::string line;
-    int line_count = 0;
-    while (std::getline(file, line)){
-        if (line_count < 2) {
-            line_count++;
-            continue;
+
+    int columns, rows, max_val;
+    file >> columns >> rows >> max_val;
+
+    std::vector<std::vector<int>> image;
+    for (int row = 0; row < rows; row++){
+        std::vector<int> row_values;
+        for(int column = 0; column < columns * 3; column++){
+            int pixel_value;
+            file >> pixel_value;
+            row_values.push_back(pixel_value);
         }
-        ascii_vector.push_back(std::vector<char>(line.begin(), line.end()));
+        ascii_vector.push_back(row_values);
     }
 
     file.close();
@@ -90,7 +96,7 @@ std::vector<std::vector<char>> create_image_v(const std::string &img_file){
 }
 
 
-void printMatrix(const std::vector<std::vector<char>>& matrix) {
+void printMatrix(const std::vector<std::vector<int>>& matrix) {
     for (const auto& row : matrix) {
         for (char c : row) {
             std::cout << c ;
